c76e7264d02419e3518fc6b60fda0903eb97ec4d
borja <borja@example.com>
2023-07-17 09:37:52 +0200

    ejercicio2 ok

--- JS/ej02/script.js
+++ JS/ej02/script.js
@@ -1,4 +1,14 @@
+let listaColores = ["red","green","yellow","orange","blue"];
+let indice = 0;
+
 function cambiar_color(){
+    
     let elem = document.getElementById("contenido");
-    elem.style.backgroundColor = "red";
+    elem.style.backgroundColor = listaColores[indice];
+    
+    if(indice==listaColores.length-1){
+        indice=0;
+    } else indice ++;
 }
+
+setInterval(cambiar_color,1000);
